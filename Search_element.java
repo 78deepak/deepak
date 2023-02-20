793f17d3d8fde103cd63f4bae076147c167807cd
78deepak <106463275+78deepak@users.noreply.github.com>
2023-02-20 22:19:44 +0530

    Create Search_element.java

--- Search_element.java
+++ Search_element.java
@@ -1,7 +1,7 @@
 public class Search_element {
     public static void main(String[] args) {
         int[] arr = {2,4,5,6,7,8};
-        int x = 12;
+        int x = 12;https://github.com/78deepak/deepak/blob/main/Search_element.java
         int ans = -1;
         for(int i = 0; i<arr.length; i++){
             if(arr[i] == x){
